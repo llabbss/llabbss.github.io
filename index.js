fdf3554b39d68f62c6672b4248bbc96a0317d3a1
lcy <401859215@qq.com>
2017-11-14 22:46:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,4 @@ var logo = document.getElementById("logo");
 logo.onclick=function(){
     alert(I am logo!)
 }
-var a={
-    name:"A",
-    age:"12"
-   }
-   a.propertype.sayName=function(){
-      alert(this.name)
-   }
+
